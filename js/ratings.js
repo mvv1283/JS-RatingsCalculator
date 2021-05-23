af599268d349dfb113e4360d36e49c16c13f8ecb
mvv1283 <35492212+mvv1283@users.noreply.github.com>
2021-05-23 14:54:52 -0400

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -15,6 +15,6 @@ const collect_ratings = () => {
   return ratings;
 }
 
-document.addEventListener('change', => {
+document.addEventListener('change', () => {
   const ratings = collect_ratings();
 });
