4caca845ca4ca9aefb78143e6cdf45e24407566e
mvv1283 <35492212+mvv1283@users.noreply.github.com>
2021-05-23 10:47:46 -0400

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -2,8 +2,9 @@ const collect_ratings = () => {
   const ratings = {'count':0, 'sum':0, 'average':0};
 
   let rating = 0;
-  const elements = document.querySelectorAll()
+  const elements = document.querySelectorAll('.rating');
   elements.forEach(element => {
+    rating = parseInt(element.id.replace('star', ''));
   }
   )
 }
